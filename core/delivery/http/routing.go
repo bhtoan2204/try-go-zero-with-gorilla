2a800cbf39d5d8ac0b9d20ad0959f8964fbefa3c
Bành Hảo Toàn <toanbh@hasaki.vn>
2026-01-28 15:49:03 +0700

    feat: add authentication requirements for room management endpoints

--- core/delivery/http/routing.go
+++ core/delivery/http/routing.go
@@ -14,14 +14,14 @@ type RoutingHandler interface {
 func (h *routingHandler) RegisterPublicHandlers(routes *gin.RouterGroup) {
 	routes.POST("/auth/login", h.Handle())
 	routes.POST("/auth/register", h.Handle())
-	routes.POST("/room/create", h.Handle())
-	routes.GET("/room/list", h.Handle())
-	routes.GET("/room/get", h.Handle())
-	routes.PUT("/room/update", h.Handle())
-	routes.DELETE("/room/delete", h.Handle())
 }
 
 func (h *routingHandler) RegisterPrivateHandlers(routes *gin.RouterGroup) {
 	routes.POST("/auth/logout", h.Handle())
 	routes.GET("/auth/profile", h.Handle())
+	routes.POST("/room/create", h.Handle())
+	routes.GET("/room/list", h.Handle())
+	routes.GET("/room/get", h.Handle())
+	routes.PUT("/room/update", h.Handle())
+	routes.DELETE("/room/delete", h.Handle())
 }
